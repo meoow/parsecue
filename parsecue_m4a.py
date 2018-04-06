3be822041b6d2b363b8fa098dd6a205975f4cc12
Leon <leon.tty1@gmail.com>
2018-04-06 08:37:00 +0800

    change to ffmpeg_mt

--- parsecue_m4a.py
+++ parsecue_m4a.py
@@ -4,4 +4,4 @@ import sys
 import parsecue_lib as pcue
 
 cue = pcue.parsecue(sys.argv[1])
-pcue.ffmpeg(cue, ['-af', 'silenceremove=1:0:-75dB:1:1:-75dB,adelay=200|200', '-c:a', 'aac', '-strict', '-2', '-b:a', '480k'], 'm4a')
+pcue.ffmpeg_mt(cue, ['-c:a', 'aac', '-strict', '-2', '-b:a', '480k'], 'm4a')
